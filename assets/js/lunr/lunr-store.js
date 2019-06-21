03dcd35f7692e1d942537ef2e8ad7b24512538fb
kac- <6b6163@gmail.com>
2019-06-21 12:32:24 +0200

    jekyll-0

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Nikotyna",
+        "excerpt":"Chemia  Alkaloid   Demon  W wielu przekazach nikotyna jest demonizowana.   Jest ok  Np. w ziemniakach   Nałóg  kromka  ","categories": [],
+        "tags": [],
+        "url": "/docs/nicotine/",
+        "teaser":null},{
+        "title": "Intro",
+        "excerpt":"Jedziemy!  ","categories": [],
+        "tags": [],
+        "url": "/fight/intro/",
+        "teaser":null},{
+        "title": "Jak to zrobić?",
+        "excerpt":"cześć heh zajrzyj na Brzozow.xyz   ","categories": ["Rzucanie","Ograniczanie"],
+        "tags": ["nikotyna","smoła"],
+        "url": "/rzucanie/ograniczanie/jak-to-jest/",
+        "teaser":null}]
